482bdc9f904215c86db2278d08c3c3e6b3215181
Issac1142-git <issacimmanuvel.t@gmail.com>
2021-01-25 22:45:09 +0530

    Initial Commit

---
+++ assets/js/todo.js
@@ -0,0 +1,41 @@
+// $("li").click(function(){
+//     if($(this).css("color") === "rgb(128, 128, 128)"){
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         })
+//     }else{
+//         $(this).css({
+//             color: "gray",
+//             textDecoration: "line-through"
+//         })
+//     }
+    
+    
+    
+// })
+
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed")
+})
+
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(1000,function(){
+        $(this).remove();
+    })
+    e.stopPropagation();
+})
+
+
+$("input[type = 'text']").keypress(function(e){
+    if(e.which === 13){
+        var todoText =  $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+    }
+})
+
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle(1000);
+})
